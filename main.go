95a1e625b6f0a0220b3a9d1dc80d4ce6f2965cdd
y <dennis.erdelean@protonmail.com>
2024-04-19 12:52:24 +0200

    api works!

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
 
+	"github.com/bitflippa27/go-crud/controllers"
+	"github.com/bitflippa27/go-crud/services"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server         *gin.Engine
+	userservice    services.UserService
+	usercontroller controllers.UserController
+	usercollection *mongo.Collection
+	ctx            context.Context
+	mongoclient    *mongo.Client
+	err            error
+)
+
+func init() {
+	ctx = context.TODO()
+
+	mongoconnection := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoclient, err := mongo.Connect(ctx, mongoconnection)
+	if err != nil {
+		log.Fatal()
+	}
+	err = mongoclient.Ping(ctx, &readpref.ReadPref{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("mongo is up and running!")
+
+	usercollection = mongoclient.Database("gocrud").Collection("users")
+	userservice = services.NewUserService(usercollection, ctx)
+	usercontroller = controllers.New(userservice)
+	server = gin.Default()
+}
+
+func main() {
+	defer mongoclient.Disconnect(ctx) // disconnect mongodb if app shuts down
+
+	basepath := server.Group("/v1") // v1/user/create
+
+	usercontroller.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":7777"))
 }
